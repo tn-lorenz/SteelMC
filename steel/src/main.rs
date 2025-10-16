4ce53de2d3d2a9e9a121114e4d01950b878b5f70
Alve Jeansson <alve.jeansson@gmail.com>
2025-10-16 19:48:04 +0200

    clean

--- steel/src/main.rs
+++ steel/src/main.rs
@@ -5,34 +5,16 @@ use steel_registry::{
     properties::{BlockStateProperties, RedstoneSide},
 };
 
-pub const REDSTONE_WIRE: Block = Block::new(
-    "redstone_wire2",
-    BlockBehaviourProperties::new(),
-    &[
-        &BlockStateProperties::EAST_REDSTONE,
-        &BlockStateProperties::NORTH_REDSTONE,
-        &BlockStateProperties::POWER,
-        &BlockStateProperties::ATTACHED,
-    ],
-)
-.with_default_state(offset!(
-    BlockStateProperties::EAST_REDSTONE => RedstoneSide::Up,
-    BlockStateProperties::NORTH_REDSTONE => RedstoneSide::None,
-    BlockStateProperties::POWER => 10,
-    BlockStateProperties::ATTACHED => BlockStateProperties::ATTACHED.index_of(false)
-));
-
 #[tokio::main]
 async fn main() {
     let mut registry = BlockRegistry::new();
 
     let start = tokio::time::Instant::now();
     vanilla_blocks::register_blocks(&mut registry);
-    registry.register(&REDSTONE_WIRE);
     println!("Time taken: {:?}", start.elapsed());
     registry.freeze();
 
-    let block = registry.by_name("redstone_wire2").unwrap();
+    let block = registry.by_name("redstone_wire").unwrap();
     let default_state_id = registry.get_default_state_id(block);
     println!(
         "default_state_id: {:?}",
