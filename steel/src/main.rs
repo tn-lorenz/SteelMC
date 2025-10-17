d2ba1c6d2ebd450a9d9180bb93f912336f03f6b2
Alve Jeansson <alve.jeansson@gmail.com>
2025-10-18 01:40:12 +0200

    gn

--- steel/src/main.rs
+++ steel/src/main.rs
@@ -9,13 +9,9 @@ async fn main() {
     println!("Time taken: {:?}", start.elapsed());
     registry.freeze();
 
-    let block = registry.by_name("waxed_copper_door").unwrap();
-    let default_state_id = registry.get_default_state_id(block);
+    let block = registry.by_name("slime_block").unwrap();
 
-    println!(
-        "default_state_id: {:?}",
-        registry.get_properties(default_state_id)
-    );
+    println!("behaviour: {:?}", block.behaviour);
 
     std::thread::sleep(std::time::Duration::from_secs(10));
 }
