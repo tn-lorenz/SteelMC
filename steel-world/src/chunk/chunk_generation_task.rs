972e123eff3fd58f2a169b399920e58e9214f05d
Alve Jeansson <alve.jeansson@gmail.com>
2025-11-09 23:21:20 +0100

    add ChunkGenerationTask

---
+++ steel-world/src/chunk/chunk_generation_task.rs
@@ -0,0 +1,33 @@
+use std::{
+    collections::HashMap,
+    sync::{Arc, atomic::AtomicBool},
+};
+
+use steel_utils::ChunkPos;
+
+use crate::chunk::{chunk_access::ChunkStatus, chunk_holder::ChunkHolder};
+
+pub struct ChunkGenerationTask {
+    pub pos: ChunkPos,
+    pub target_status: ChunkStatus,
+    pub scheduled_status: Option<ChunkStatus>,
+    pub marked_for_cancel: AtomicBool,
+
+    pub neighbor_ready: Vec<Box<dyn Future<Output = ()> + Send>>,
+    pub cache: HashMap<ChunkStatus, Arc<ChunkHolder>>,
+    pub needs_generation: bool,
+}
+
+impl ChunkGenerationTask {
+    pub fn new(pos: ChunkPos, target_status: ChunkStatus) -> Self {
+        Self {
+            pos,
+            target_status,
+            scheduled_status: None,
+            marked_for_cancel: AtomicBool::new(false),
+            neighbor_ready: Vec::new(),
+            cache: HashMap::new(),
+            needs_generation: true,
+        }
+    }
+}
