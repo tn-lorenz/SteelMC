f539e012741dc3ce88d000a89e93374abf49c11d
Richard Marshall <r.s.sutton@hotmail.co.uk>
2026-06-25 18:53:39 +0100

    Fix rsa public key encoding (#231)

--- steel-crypto/src/key_store.rs
+++ steel-crypto/src/key_store.rs
@@ -1,5 +1,5 @@
 //! This module contains the `KeyStore` struct, which is used to store the server's encryption keys.
-use rsa::{RsaPrivateKey, traits::PublicKeyParts};
+use rsa::{RsaPrivateKey, RsaPublicKey};
 
 /// A struct that stores the server's encryption keys.
 pub struct KeyStore {
@@ -16,10 +16,9 @@ impl KeyStore {
         log::debug!("Creating encryption keys...");
         let private_key = Self::generate_private_key();
 
-        let public_key_der = rsa_der::public_key_to_der(
-            &private_key.n().to_be_bytes(),
-            &private_key.e().to_be_bytes(),
-        );
+        let public_key = RsaPublicKey::from(&private_key);
+        let public_key_der =
+            crate::public_key_to_bytes(&public_key).expect("Failed to encode public key");
 
         Self {
             private_key,
@@ -31,7 +30,18 @@ impl KeyStore {
         // Found out that OsRng is faster than rand::thread_rng here
         let mut rng = rand::rng();
 
-        // let pub_key = RsaPublicKey::from(&priv_key);
         RsaPrivateKey::new(&mut rng, 1024).expect("Failed to generate a key")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn public_key_der_round_trips() {
+        let ks = KeyStore::create();
+        let decoded = crate::public_key_from_bytes(&ks.public_key_der).unwrap();
+        assert_eq!(decoded, RsaPublicKey::from(&ks.private_key));
     }
 }
