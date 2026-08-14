1401787192763d31a983be94560dbadc2f96ecd8
tn-lorenz <contact@tn-lorenz.de>
2026-08-14 22:09:09 +0200

    stole some purdze code

--- steel-core/src/behavior/items/fishing_rod.rs
+++ steel-core/src/behavior/items/fishing_rod.rs
@@ -1,14 +1,16 @@
 use crate::behavior::{InteractionResult, ItemBehavior, UseItemContext};
 use crate::entity::projectile::fishing_hook::{FishingHook, FishingHookState};
-use crate::entity::{Entity, Projectile};
-use crate::world::World;
+use crate::entity::{next_entity_id, Entity, Projectile, SharedEntity};
 use glam::DVec3;
 use rand::{RngExt, rng};
-use std::sync::Weak;
+use std::sync::Arc;
 use steel_macros::item_behavior;
+use steel_protocol::packets::game::SoundSource;
 use steel_registry::sound_events::{ENTITY_FISHING_BOBBER_RETRIEVE, ENTITY_FISHING_BOBBER_THROW};
 use steel_registry::vanilla_entities;
 use steel_utils::locks::SyncMutex;
+
+const SHOOT_POWER: f32 = 1.5;
 
 /// literally self-explanatory
 #[item_behavior]
@@ -17,35 +19,57 @@ pub struct FishingRodItem;
 impl ItemBehavior for FishingRodItem {
     fn use_item(&self, context: &mut UseItemContext) -> InteractionResult {
         let player = context.player;
+        let world = context.world;
         let infinite_materials = context.player.has_infinite_materials();
+
+        let pitch = 0.4 / (rng().random::<f32>() * 0.4 + 0.8);
+
         if let Some(fishing) = &player.fishing {
             context.inv.with_item(|item| {
                 let damage = fishing.retrieve(item);
                 item.hurt_and_break(damage, infinite_materials);
             });
 
-            player.play_sound(
+            world.play_sound_at(
                 &ENTITY_FISHING_BOBBER_RETRIEVE,
+                SoundSource::Neutral,
+                player.position(),
                 1.0,
-                0.4 / (rng().random::<f32>() * 0.4 + 0.8),
+                pitch,
+                None,
             );
             // TODO: vibration
         } else {
-            player.play_sound(
+            world.play_sound_at(
                 &ENTITY_FISHING_BOBBER_THROW,
+                SoundSource::Neutral,
+                player.position(),
                 0.5,
-                0.4 / (rng().random::<f32>() * 0.4 + 0.8),
+                pitch,
+                None,
             );
+
+            let player_pos = player.position();
+            let spawn_pos = DVec3::new(player_pos.x, player.get_eye_y() - 0.1, player_pos.z);
 
             let hook = FishingHook::new(
                 &vanilla_entities::FISHING_BOBBER,
-                1,
-                DVec3::ZERO,
-                Weak::<World>::new(),
+                next_entity_id(),
+                spawn_pos,
+                Arc::downgrade(world),
                 SyncMutex::new(FishingHookState::new(0, 0)),
             );
 
-            hook.shoot(DVec3::new(0.0, 0.0, 1.0), 1.5, 1.0);
+            if let Some(owner) = world.players.get_by_uuid(&player.gameprofile.id) {
+                let owner: SharedEntity = owner;
+                hook.set_owner_entity(Some(&owner));
+            } else {
+                hook.set_owner_uuid(Some(player.gameprofile.id));
+            }
+            //hook.set_item_clamped(thrown_item);
+
+            let (yaw, player_pitch) = player.rotation();
+            hook.shoot_from_rotation(player, player_pitch, yaw, 0.0, SHOOT_POWER, 1.0);
             // TODO: award stat
             // TODO: vibration
         }
