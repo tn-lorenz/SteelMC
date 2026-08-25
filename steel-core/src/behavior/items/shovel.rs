b3cdff21f240b9dd6c3984502e3b7215590660d5
AnthonyDuong1 <100894923+AnthonyDuong1@users.noreply.github.com>
2026-08-24 22:10:49 -0400

    feat: add shovel flattening sound (#468)
    Co-authored-by: coco875 <59367621+coco875@users.noreply.github.com>

--- steel-core/src/behavior/items/shovel.rs
+++ steel-core/src/behavior/items/shovel.rs
@@ -5,7 +5,7 @@ use steel_registry::{
         block_state_ext::BlockStateExt,
         properties::{BlockStateProperties, BoolProperty},
     },
-    level_events,
+    level_events, sound_events,
     vanilla_block_tags::BlockTag,
     vanilla_blocks, vanilla_game_events,
 };
@@ -14,6 +14,7 @@ use steel_utils::types::UpdateFlags;
 
 use crate::{
     behavior::{InteractionResult, ItemBehavior, UseOnContext},
+    entity::Entity,
     world::game_event::GameEventContext,
 };
 
@@ -50,7 +51,13 @@ impl ItemBehavior for ShovelItem {
             {
                 return InteractionResult::Pass;
             }
-            // TODO: Play SoundEvents.SHOVEL_FLATTEN
+            context.world.play_block_sound(
+                &sound_events::ITEM_SHOVEL_FLATTEN,
+                context.hit_result.block_pos,
+                1.0,
+                1.0,
+                Some(context.player.id()),
+            );
             let infinite_materials = context.player.has_infinite_materials();
             context
                 .inv
