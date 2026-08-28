ff6c0205e5418f7316d103ab3a75a4182fe843bb
tn-lorenz <contact@tn-lorenz.de>
2026-08-28 17:18:34 +0200

    fmt

--- steel-core/src/entity/entities/mod.rs
+++ steel-core/src/entity/entities/mod.rs
@@ -9,7 +9,9 @@ pub use mobs::passive::{ChickenEntity, CowEntity, PigEntity, SheepEntity};
 pub use objects::display_ui::{BlockDisplayEntity, ItemFrameEntity, LeashFenceKnotEntity};
 pub use objects::explosives::EndCrystalEntity;
 pub use objects::items::{ExperienceOrbEntity, FallingBlockEntity, ItemEntity};
-pub use objects::projectiles::{EnderPearlEntity, FishingHookEntity, FireworkRocketEntity, ThrownEggEntity};
+pub use objects::projectiles::{
+    EnderPearlEntity, FireworkRocketEntity, FishingHookEntity, ThrownEggEntity,
+};
 pub use objects::technical::MarkerEntity;
 pub use objects::vehicles::ChestMinecartEntity;
 pub use raw::RawEntity;
