721f4b0a56b7719b52c6dae42ded92f11d48c107
tn-lorenz <contact@tn-lorenz.de>
2026-09-03 12:55:41 +0200

    address clicky's comment

--- steel-core/src/behavior/items/fishing_rod.rs
+++ steel-core/src/behavior/items/fishing_rod.rs
@@ -6,6 +6,7 @@ use rand::{RngExt, rng};
 use std::sync::Arc;
 use steel_macros::item_behavior;
 use steel_protocol::packets::game::SoundSource;
+use steel_registry::enchantment_effect::EnchantmentEffectComponent;
 use steel_registry::sound_events::{ENTITY_FISHING_BOBBER_RETRIEVE, ENTITY_FISHING_BOBBER_THROW};
 use steel_registry::stat::vanilla_stat_types;
 use steel_registry::{vanilla_entities, vanilla_items};
@@ -60,9 +61,15 @@ impl ItemBehavior for FishingRodItem {
             let player_pos = player.position();
             let spawn_pos = DVec3::new(player_pos.x, player.get_eye_y() - 0.1, player_pos.z);
 
-            // FIXME: we don't have enchantments yet, so I can't get the current `luck` or `lure_speed` of `rod`
-            let luck = 0;
-            let lure_speed = 0;
+            let luck = item.apply_unconditional_enchantment_value_effects(
+                EnchantmentEffectComponent::FishingLuckBonus,
+                0.0,
+            );
+
+            let lure_speed = item.apply_unconditional_enchantment_value_effects(
+                EnchantmentEffectComponent::FishingTimeReduction,
+                0.0,
+            );
 
             let hook = Arc::new(FishingHookEntity::new(
                 &vanilla_entities::FISHING_BOBBER,
@@ -76,7 +83,7 @@ impl ItemBehavior for FishingRodItem {
                 .get_by_uuid(&player.gameprofile.id)
                 .expect("Failed to obtain `player_arc` for launching fishing hook: `player` must be registered in the world.");
 
-            hook.shoot_from_player(&player_arc, luck, lure_speed);
+            hook.shoot_from_player(&player_arc, luck as i32, lure_speed as i32);
 
             let entity: SharedEntity = hook;
 
